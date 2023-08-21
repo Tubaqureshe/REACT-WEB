00fcf35da1ab5eb9c9653fa13b0afcfdba8ba628
Soomi <125480789+Tubaqureshe@users.noreply.github.com>
2023-08-21 12:51:41 +0500

    Update LoginForm.jsx

--- client/src/Guset/components/CustomForm/LoginForm.jsx
+++ client/src/Guset/components/CustomForm/LoginForm.jsx
@@ -1,76 +1,51 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { GlobalContext } from '../../../Context/context';
+import React, { useContext, useState } from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { GlobalContext } from '../../../Context/context'
 import Swal from 'sweetalert2';
 
 
-export default function AuthForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [isLogin, setIsLogin] = useState(true);
+export default function LoginForm() {
 
-    const { state, dispatch } = useContext(GlobalContext);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const handleAuth = (e) => {
+    const { state, dispatch } = useContext(GlobalContext)
+
+    const loginUser = (e) => {
         e.preventDefault();
-
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Username:', username);
-
-        const payload = { email, password, username };
-
-        const apiUrl = isLogin ? 'http://localhost:3000/api/login' : 'http://localhost:3000/api/signup';
-
-        axios.post(apiUrl, payload)
+    
+        const payload = { email, password }
+    
+        axios.post('http://localhost:3000/api/login', payload)
             .then((json) => {
-                console.log('Response Data:', json.data); // Log the response data
-
-                if (isLogin) {
-                    Cookies.set('token', json.data.token);
-                    dispatch({
-                        type: 'USER_LOGIN',
-                        token: json.data.token,
-                    });
-
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Logged In',
-                        text: 'You have successfully logged in!',
-                    });
-                } else {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Sign Up Successful',
-                        text: 'You have successfully signed up! You can now log in.',
-                    });
-                }
+                Cookies.set('token', json.data.token)
+                dispatch({
+                    type: "USER_LOGIN",
+                    token: json.data.token
+                });
+    
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Logged In',
+                    text: 'You have successfully logged in!',
+                });
             })
-            .catch((err) => {
+            .catch(err => {
                 console.log(err);
                 Swal.fire({
                     icon: 'error',
-                    title: isLogin ? 'Login Failed' : 'Sign Up Failed',
-                    text: `There was an error ${isLogin ? 'logging in' : 'signing up'}. Please check your credentials and try again.`,
+                    title: 'Login Failed',
+                    text: 'There was an error logging in. Please check your credentials and try again.',
                 });
             });
     }
+    
 
     return (
-        <div className={`flip-card__auth ${isLogin ? 'login' : 'signup'}`}>
-            <div className="title">{isLogin ? 'Log in' : 'Sign up'}</div>
-            <form className="flip-card__form" onSubmit={handleAuth}>
-                {!isLogin && (
-                    <input
-                        className="flip-card__input"
-                        placeholder="Name"
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                )}
+        <div className="flip-card__front">
+            <div className="title">Log in</div>
+            <form className="flip-card__form" onSubmit={loginUser}>
                 <input
                     className="flip-card__input"
                     name="email"
@@ -87,21 +62,8 @@ export default function AuthForm() {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="flip-card__btn">{isLogin ? "Let's go!" : "Confirm!"}</button>
+                <button className="flip-card__btn">Let`s go!</button>
             </form>
-            <div className="flip-card__auth-toggle">
-                {isLogin ? (
-                    <>
-                        Don't have an account?{' '}
-                        <button onClick={() => setIsLogin(false)}>Sign Up</button>
-                    </>
-                ) : (
-                    <>
-                        Already have an account?{' '}
-                        <button onClick={() => setIsLogin(true)}>Log In</button>
-                    </>
-                )}
-            </div>
         </div>
-    );
-}
+    )
+} 
