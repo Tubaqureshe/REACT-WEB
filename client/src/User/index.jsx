ebaf17d56da83e5fc5dbca843fecb37ee87bdc44
Soomi <125480789+Tubaqureshe@users.noreply.github.com>
2023-08-21 10:53:28 +0500

    Update index.jsx

--- client/src/User/index.jsx
+++ client/src/User/index.jsx
@@ -6,32 +6,22 @@ import Category from './Pages/Category'
 import Home from './Pages/Home'
 import Products from './Pages/Products'
 import ProductsByBrand from './pages/ProductsByBrand'
-import Profile from './Pages/Profile'
 import ProductsByCategory from './Pages/ProductsByCategory'
 import ProductPage from './pages/ProductPage'
 import CheckoutPage from './pages/CheckoutPage'
-// import CustomCart from './pages/CustomCart'
-// import Cart from './Pages/Cart'
-// import Cart from './pages/Cart'
 import { Navigate, Route, Routes } from "react-router-dom";
-
-
 export default function User() {
 
     return (
-
         <>
             <UserNav />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/brands" element={<Brands />} />
-                {/* <Route path="/cart" element={<Cart />} /> */}
-                          
+                <Route path="/brands" element={<Brands />} />             
                 <Route path="/brands/:BrandName" element={<ProductsByBrand />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/products/:_id" element={<ProductPage />} />
                 <Route path="/category" element={<Category />} />
-                <Route path="/profile" element={<Profile />} />
                 <Route path="/category/:CategoryName" element={<ProductsByCategory />} />
                 <Route path="/checkout" element={<CheckoutPage />} />
                 <Route path="*" element={<Navigate to='/' replace={true} />} />
